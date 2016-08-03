301f9a45024a72c17113483c1bddbd275ca3b38d
Jamie Robert Dawson <jamierobertdawson@gmail.com>
2016-08-03 17:02:26 -0400

    Drag and Drop files on the Form. Find the path to the files you drop.

---
+++ C_DragAndDrop/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C_DragAndDrop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //this lets you drag files into the form.
+        private void panel1_DragOver(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+        }
+        //This brings up a message box showing the path to the file.
+        private void panel1_DragDrop(object sender, DragEventArgs e)
+        {
+          string[] files = e.Data.GetData(DataFormats.FileDrop) as String[];
+            foreach (string s in files)
+                MessageBox.Show(s);
+        }
+    }
+}
